9a1508530c80c7ba0d9de5920b25661cd2710f3b
vansh-avidly <vans.gandhi@avidlyagency.com>
2025-03-26 14:08:24 +0530

    Added a form and record listing

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
+require("dotenv").config();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.HS_ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get("/", async (req, res) => {
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    const { data } = await axios.get(
+      "https://api.hubapi.com/crm/v3/objects/2-42464116?properties=object_id,name,bio",
+      {
+        headers,
+      }
+    );
+    res.render("homepage", { data: data.results });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get("/update-cobj", (req, res) => {
+  res.render("updates", {
+    title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+  });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post("/update-cobj", async (req, res) => {
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    await axios.post(
+      "https://api.hubapi.com/crm/v3/objects/2-42464116",
+      {
+        properties: { ...req.body },
+      },
+      {
+        headers,
+      }
+    );
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -66,6 +109,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
